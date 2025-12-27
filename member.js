55427125a23af3fbe607c71bcc665a34dfd77d6b
aa0978033119-max <aa0978033119@gmail.com>
2025-12-28 01:22:48 +0800

    Update member.js

--- member.js
+++ member.js
@@ -11,16 +11,17 @@ function showSection(sectionId) {
     document.getElementById(sectionId).classList.add('active');
   }
   
-  // 登出
+// 登出
   function logout() {
   localStorage.removeItem("user");   // 登出
   alert("已登出");
   window.location.href = "index.html";
 }
 
-//登入
+// 登入
 function loginAsGuest() {
   localStorage.setItem("user", "STANDARD 會員");
   alert("登入成功");
   location.reload(); // 重新整理，使 header 更新
 }
+
