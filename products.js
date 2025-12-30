6bcfabc3052684e63c2499435cde223b70915b8e
wen052883 <hsiaowendy0528@gmail.com>
2025-12-30 13:08:09 +0800

    Update and rename clothes.js to products.js

---
+++ products.js
@@ -0,0 +1,21 @@
+const products = [
+  {
+    id: 1,
+    name: "STANDARD DAY TEE",
+    price: 1280,
+    desc: "簡約日常 T-shirt",
+    detail: "100% Cotton / Made in Taiwan",
+    sizes: ["S", "M", "L"],
+    images: ["images/p1-1.jpg"]
+  },
+  {
+    id: 2,
+    name: "STANDARD DAY SHIRT",
+    price: 1980,
+    desc: "寬版襯衫",
+    detail: "落肩設計，適合四季",
+    sizes: ["M", "L"],
+    images: ["images/p2-1.jpg"]
+  },
+ 
+];
