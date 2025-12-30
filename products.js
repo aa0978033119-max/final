2764853a9f466a2cac9846a7a43484c146e34b83
wen052883 <hsiaowendy0528@gmail.com>
2025-12-30 14:35:17 +0800

    Update products.js

--- products.js
+++ products.js
@@ -1,21 +1,146 @@
 const products = [
   {
     id: 1,
-    name: "STANDARD DAY TEE",
+    name: "夢幻粉色大衣",
     price: 1280,
     desc: "簡約日常 T-shirt",
     detail: "100% Cotton / Made in Taiwan",
     sizes: ["S", "M", "L"],
     images: ["images/p1-1.jpg"]
   },
-  {
+   {
     id: 2,
-    name: "STANDARD DAY SHIRT",
-    price: 1980,
-    desc: "寬版襯衫",
-    detail: "落肩設計，適合四季",
-    sizes: ["M", "L"],
-    images: ["images/p2-1.jpg"]
+    name: "百搭基礎牛仔褲",
+    price: 1280,
+    desc: "簡約日常 T-shirt",
+    detail: "100% Cotton / Made in Taiwan",
+    sizes: ["S", "M", "L"],
+    images: ["images/p1-1.jpg"]
   },
- 
+   {
+    id: 3,
+    name: "時尚週限定條紋長裙",
+    price: 1280,
+    desc: "簡約日常 T-shirt",
+    detail: "100% Cotton / Made in Taiwan",
+    sizes: ["S", "M", "L"],
+    images: ["images/p1-1.jpg"]
+  },
+    {
+    id: 4,
+    name: "學院格紋顯身短裙",
+    price: 1280,
+    desc: "簡約日常 T-shirt",
+    detail: "100% Cotton / Made in Taiwan",
+    sizes: ["S", "M", "L"],
+    images: ["images/p1-1.jpg"]
+  },
+    {
+    id: 5,
+    name: "質感牛仔夾克",
+    price: 1100,
+    desc: "簡約日常 T-shirt",
+    detail: "100% Cotton / Made in Taiwan",
+    sizes: ["S", "M", "L"],
+    images: ["images/p1-1.jpg"]
+  },
+    {
+    id: 6,
+    name: "質感黑色牛仔夾克",
+    price: 1280,
+    desc: "簡約日常 T-shirt",
+    detail: "100% Cotton / Made in Taiwan",
+    sizes: ["S", "M", "L"],
+    images: ["images/p1-1.jpg"]
+  },
+    {
+    id: 7,
+    name: "紳士透膚襯衫",
+    price: 1280,
+    desc: "簡約日常 T-shirt",
+    detail: "100% Cotton / Made in Taiwan",
+    sizes: ["S", "M", "L"],
+    images: ["images/p1-1.jpg"]
+  },
+    {
+    id: 8,
+    name: "超前衛運動上衣",
+    price: 590,
+    desc: "簡約日常 T-shirt",
+    detail: "100% Cotton / Made in Taiwan",
+    sizes: ["S", "M", "L"],
+    images: ["images/p1-1.jpg"]
+  },
+    {
+    id: 9,
+    name: "象牙白打底上衣",
+    price: 1280,
+    desc: "簡約日常 T-shirt",
+    detail: "100% Cotton / Made in Taiwan",
+    sizes: ["S", "M", "L"],
+    images: ["images/p1-1.jpg"]
+  },
+    {
+    id: 10,
+    name: "復古垂感束腳工裝褲",
+    price: 1280,
+    desc: "簡約日常 T-shirt",
+    detail: "100% Cotton / Made in Taiwan",
+    sizes: ["S", "M", "L"],
+    images: ["images/p1-1.jpg"]
+  },
+    {
+    id: 11,
+    name: "男裝天絲彈力直筒卡其褲",
+    price: 1280,
+    desc: "簡約日常 T-shirt",
+    detail: "100% Cotton / Made in Taiwan",
+    sizes: ["S", "M", "L"],
+    images: ["images/p1-1.jpg"]
+  },
+    {
+    id: 1,
+    name: "夢幻粉色大衣",
+    price: 1280,
+    desc: "簡約日常 T-shirt",
+    detail: "100% Cotton / Made in Taiwan",
+    sizes: ["S", "M", "L"],
+    images: ["images/p1-1.jpg"]
+  },
+    {
+    id: 12,
+    name: "亮鑽百褶網紗白色長裙",
+    price: 1280,
+    desc: "簡約日常 T-shirt",
+    detail: "100% Cotton / Made in Taiwan",
+    sizes: ["S", "M", "L"],
+    images: ["images/p1-1.jpg"]
+  },
+    {
+    id: 13,
+    name: "歐美紫色西裝外套",
+    price: 1280,
+    desc: "簡約日常 T-shirt",
+    detail: "100% Cotton / Made in Taiwan",
+    sizes: ["S", "M", "L"],
+    images: ["images/p1-1.jpg"]
+  },
+    {
+    id: 14,
+    name: "斜肩氣質荷葉邊上衣短板聯名渲染上衣15",
+    price: 1280,
+    desc: "簡約日常 T-shirt",
+    detail: "100% Cotton / Made in Taiwan",
+    sizes: ["S", "M", "L"],
+    images: ["images/p1-1.jpg"]
+  },
+   {
+    id: 15,
+    name: "短板聯名渲染上衣",
+    price: 1280,
+    desc: "簡約日常 T-shirt",
+    detail: "100% Cotton / Made in Taiwan",
+    sizes: ["S", "M", "L"],
+    images: ["images/p1-1.jpg"]
+  },
 ];
