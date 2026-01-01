280a8b5cd73d4cac8a28a737369335204f867e60
wen052883 <hsiaowendy0528@gmail.com>
2026-01-02 06:45:36 +0800

    Update products.js

--- products.js
+++ products.js
@@ -21,7 +21,7 @@ const products = [
     id: 3,
     name: "時尚週限定條紋長裙",
     price: 1280,
-    desc: "時髦剪裁搭配黑白條紋，展現精緻氣息",
+    desc: "時髦剪裁搭配深色條紋，展現精緻氣息",
     detail: "100% Cotton / Made in Taiwan",
     sizes: ["F"],
     images: ["images/03.jpg"]
@@ -49,7 +49,7 @@ const products = [
     name: "質感黑色牛仔夾克",
     price: 1280,
     desc: "牛仔元素永不過時，顯身材比例的最佳單品",
-    detail: "100% Cotton / Made in Taiwan",
+    detail: "100% Cotton / Made in Taiwan", 
     sizes: ["S", "M", "L"],
     images: ["images/06.jpg"]
   },
@@ -57,45 +57,45 @@ const products = [
     id: 7,
     name: "紳士透膚襯衫",
     price: 1280,
-    desc: "簡約日常 T-shirt",
+    desc: "簡約日常的百搭單品",
     detail: "100% Cotton / Made in Taiwan",
-    sizes: ["S", "M", "L"],
+    sizes: ["S", "M", "L","XL"],
     images: ["images/07.jpg"]
   },
     {
     id: 8,
     name: "超前衛運動上衣",
     price: 590,
-    desc: "簡約日常 T-shirt",
+    desc: "絕不會出錯的T-shirt，本店最熱賣的上衣款式",
     detail: "100% Cotton / Made in Taiwan",
-    sizes: ["S", "M", "L"],
+    sizes: ["S", "M", "L","XL"],      
     images: ["images/08.jpg"]
   },
     {
     id: 9,
     name: "象牙白打底上衣",
     price: 1280,
-    desc: "簡約日常 T-shirt",
+    desc: "品牌打版T-shirt，簡單但不普通，值得一試的質感上衣",
     detail: "100% Cotton / Made in Taiwan",
-    sizes: ["S", "M", "L"],
+    sizes: ["S", "M", "L","XL"],
     images: ["images/09.jpg"]
   },
     {
     id: 10,
     name: "復古垂感束腳工裝褲",
     price: 1280,
-    desc: "簡約日常 T-shirt",
+    desc: "垂墜感與挺度同時兼具的工裝褲，品牌隱藏的熱賣款",
     detail: "100% Cotton / Made in Taiwan",
-    sizes: ["S", "M", "L"],
+    sizes: ["S", "M", "L","XL"],
     images: ["images/10.jpg"]
   },
     {
     id: 11,
     name: "男裝天絲彈力直筒卡其褲",
     price: 1280,
-    desc: "簡約日常 T-shirt",
+    desc: "低調百搭的褲型，穿起來舒適度極佳",
     detail: "100% Cotton / Made in Taiwan",
-    sizes: ["S", "M", "L"],
+    sizes: ["S", "M", "L","XL"],
     images: ["images/11.jpg"]
     },
   
@@ -103,36 +103,36 @@ const products = [
     id: 12,
     name: "亮鑽百褶網紗白色長裙",
     price: 1280,
-    desc: "簡約日常 T-shirt",
+    desc: "充滿亮點的裙裝款式，為穿搭創造華麗的個性",
     detail: "100% Cotton / Made in Taiwan",
-    sizes: ["S", "M", "L"],
+    sizes: ["F"],
     images: ["images/12.jpg"]
   },
     {
     id: 13,
     name: "歐美紫色西裝外套",
     price: 1280,
-    desc: "簡約日常 T-shirt",
+    desc: "穿上自帶氣場的西裝外套，非常時髦的剪裁設計",
     detail: "100% Cotton / Made in Taiwan",
-    sizes: ["S", "M", "L"],
+    sizes: ["F"],
     images: ["images/13.jpg"]
   },
     {
     id: 14,
-    name: "斜肩氣質荷葉邊上衣短板聯名渲染上衣15",
+    name: "斜肩氣質荷葉邊上衣",
     price: 1280,
-    desc: "簡約日常 T-shirt",
+    desc: "除了斜肩也能拉成平口上衣來穿，依照心情自由變換穿法",
     detail: "100% Cotton / Made in Taiwan",
-    sizes: ["S", "M", "L"],
+    sizes: ["F"],
     images: ["images/14.jpg"]
   },
    {
     id: 15,
     name: "短板聯名渲染上衣",
     price: 1280,
-    desc: "簡約日常 T-shirt",
+    desc: "穿上帶你回到千禧年，渲染設計獨具特色",
     detail: "100% Cotton / Made in Taiwan",
-    sizes: ["S", "M", "L"],
+    sizes: ["F"],
     images: ["images/15.jpg"]
   },
 ];
