0df70109aab877b7247ce15f5ff205c8288ec607
aa0978033119-max <aa0978033119@gmail.com>
2026-01-02 22:49:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   const banners = [
-    { img: "images/banner1.jpg", title: "NEW ARRIVAL", desc: "秋冬新品 8 折起" },
-    { img: "images/banner2.jpg", title: "SALE", desc: "限時優惠 20% OFF" },
-    { img: "images/banner3.jpg", title: "HOT ITEMS", desc: "人氣熱銷商品" }
+    { img: "images/02.jpg", title: "NEW ARRIVAL", desc: "秋冬新品 8 折起" },
+    { img: "images/03.jpg", title: "SALE", desc: "限時優惠 20% OFF" },
+    { img: "images/04.jpg", title: "HOT ITEMS", desc: "人氣熱銷商品" }
   ];
 
   let currentIndex = 0;
