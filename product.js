7663d1299461653e6219c4b911c76dfa2118c946
wen052883 <hsiaowendy0528@gmail.com>
2026-01-05 07:36:25 +0800

    Update product.js

--- product.js
+++ product.js
@@ -67,7 +67,9 @@ document.addEventListener("DOMContentLoaded", () => {
   //送貨及付款方式
   const shippingContainer = document.getElementById("shipping");
   shippingContainer.innerHTML = `
-    <p>宅配 7-11 全家</p>
-    <p>Apple Pay LINE Pay 銀行轉帳 信用卡 超商取貨付款</p>
+    <h4>配送方式</h4>
+    <p>宅配 / 7-11 / 全家</p>
+    <h4>付款方式</h4>
+    <p>Apple Pay / LINE Pay / 銀行轉帳 / 信用卡 / 超商取貨付款</p>
   `;
 });
